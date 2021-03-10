fe1be33e241ece12f2af55160f5486e76b94f0e9
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-03-11 08:58:50 +0900

    Update

--- js/task02.js
+++ js/task02.js
@@ -1,4 +1,6 @@
-const ul = document.querySelector('.lists');
+const ul_1 = document.querySelector('.list_1');
+const ul_2 = document.querySelector('.list_2');
+const ul_3 = document.querySelector('.list_3');
 
 const li = document.createElement('li');
 
@@ -7,7 +9,13 @@ image.src = "bookmark.png";
 image.setAttribute('alt', 'ブックマーク')
 
 const link = document.createElement('a');
-link.href = "1.html";
+link.setAttribute("href", "1.html")
 link.innerHTML = "これです"
 
-ul.appendChild(li).appendChild(link).appendChild(image);
+// ul_1.appendChild(image);
+// ul_2.appendChild(link);
+// ul_3.appendChild(li);
+
+ul_1.appendChild(li).appendChild(link).appendChild(image);
+
+//li > a > img > text
