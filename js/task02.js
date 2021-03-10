33e56a50f6a72bc1ae65832e535b3a57e2ae6c88
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-03-11 08:59:52 +0900

    Remove unnecessary code, change class name

--- js/task02.js
+++ js/task02.js
@@ -1,6 +1,4 @@
-const ul_1 = document.querySelector('.list_1');
-const ul_2 = document.querySelector('.list_2');
-const ul_3 = document.querySelector('.list_3');
+const ul = document.querySelector('.lists');
 
 const li = document.createElement('li');
 
@@ -12,10 +10,4 @@ const link = document.createElement('a');
 link.href = "1.html";
 link.innerHTML = "これです"
 
-// ul_1.appendChild(image);
-// ul_2.appendChild(link);
-// ul_3.appendChild(li);
-
-ul_1.appendChild(li).appendChild(link).appendChild(image);
-
-//li > a > img > text
+ul.appendChild(li).appendChild(link).appendChild(image);
