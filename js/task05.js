10a47484616cd8e79b78790bb1892d4dce559ed1
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-03-11 08:57:46 +0900

    Remove unnecessary code, change class name

--- js/task05.js
+++ js/task05.js
@@ -3,7 +3,7 @@ const data = [
   {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
 ]
 
-var promise = new Promise((resolve,reject) => {
+const promise = new Promise((resolve,reject) => {
 
   resolve(data);
 
@@ -14,7 +14,6 @@ const ul = document.querySelector('.list');
 promise.then(response => {
   response.map(el => {
     let str = `<li><a href="${el.to}"><img src="${el.img}" alt="${el.alt}">${el.text}</a></li>`;
-    console.log(str)
     ul.insertAdjacentHTML('afterbegin', str);
   })
 })
