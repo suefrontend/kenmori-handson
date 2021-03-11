4c1a7ff0fd7f2fca8bab26d3c2507b9f7fa82185
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-03-11 10:03:14 +0900

    Remove factory function, create two new objects

--- js/task03.js
+++ js/task03.js
@@ -1,17 +1,17 @@
-function List(href, src, text) {
-  return {
-    href,
-    src,
-    text
-  }
-}
-const item1 = new List("a1.html", "/img/bookmark.png", "a1");
-const item2 = new List("a2.html", "/img/bookmark.png", "a2");
+const item1 = {};
+item1.href = "a1.html";
+item1.src = "/img/bookmark.png";
+item1.text = "a1";
 
-const ul = document.querySelector('.list');
+const item2 = {};
+item2.href = "a2.html";
+item2.src = "/img/bookmark.png";
+item2.text = "a2";
 
-const code =  `
+const ul = document.querySelector('#lists');
+
+const markup = `
   <li><a href=${item1.href}><img src=${item1.src}>${item1.text}</a></li>
-  <li><a href=${item2.href}><img src=${item2.src}>${item2.text}</a></li>`
-;
-ul.innerHTML = code;
+  <li><a href=${item2.href}><img src=${item2.src}>${item2.text}</a></li>
+`;
+ul.innerHTML = markup;
