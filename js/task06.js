fcc09c39a3d631eb53ab053f7428b234193b42a8
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-03-11 16:16:39 +0900

    Updated task06

---
+++ js/task06.js
@@ -0,0 +1,24 @@
+const data = [
+  {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+  {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+]
+
+const promise = new Promise((resolve,reject) => {
+  setTimeout(() => {
+    resolve(data);
+  }, 3000)
+});
+
+const ul = document.querySelector('#lists');
+
+promise.then(response => {
+
+  response.map(data => {
+
+    const {to, img, alt, text} = data;
+
+    const markup = `<li><a href="${to}"><img src="${img}" alt="${alt}">${text}</a></li>`;
+    ul.insertAdjacentHTML('afterbegin', markup);
+  })
+
+})
