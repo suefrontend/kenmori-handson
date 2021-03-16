6f47ec4caa5a315ecea7127cafaf42bf362f77ab
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-03-16 20:05:22 +0900

    Update task08

---
+++ js/task08.js
@@ -0,0 +1,29 @@
+const data = [
+  {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+  {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+]
+
+const ul = document.querySelector('#lists');
+
+// loader画像
+const loader = document.createElement('img');
+loader.src = "./img/loading-circle.gif";
+
+//loading画像
+ul.appendChild(loader);
+
+const promise = new Promise((resolve,reject) => {
+
+    // いままでresolveとしていたところでrejectを実行
+    //3秒後にrejectを実行
+    setTimeout(() => {
+      reject(data);
+    }, 3000)
+
+});
+
+//thenでその値をコンソール出力してください
+// エラーを起こしてcatchでエラーをキャッチ
+promise.catch((error) => {
+  console.log("エラーが起こりました", error);
+})
