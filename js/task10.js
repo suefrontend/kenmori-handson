7953273313c54b00da1d6e871bca92d18eec7b2b
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-03-19 14:23:49 +0900

    Update task10

---
+++ js/task10.js
@@ -0,0 +1,43 @@
+const data = [
+  {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+  {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+]
+
+const ul = document.querySelector('#lists');
+
+// loader画像
+const loader = document.createElement('img');
+loader.src = "./img/loading-circle.gif";
+
+function fetchData() {
+
+  return new Promise((resolve,reject) => {
+      setTimeout(() => {
+        resolve(data);
+      }, 3000)
+  });
+
+}
+
+async function renderData() {
+
+  try {
+    //resolveになるまでの間にloading画像を出す
+    ul.appendChild(loader);
+
+    await fetchData(data);
+
+    const markup = data.reduce((prev, current) => {
+      return `${prev}<li><a href="${current.to}"><img src="${current.img}" alt="${current.alt}">${current.text}</a></li>`;
+    }, "")
+    ul.innerHTML = markup;
+  }
+  catch(error) {
+    console.log(error);
+  }
+  finally {
+    ul.removeChild(loader);
+  }
+}
+
+renderData();
