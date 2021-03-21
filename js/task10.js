743a0a4aa4fb5a2532b83a93a21add46ffc42fa9
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-03-21 16:13:13 +0900

    fix: Add data to resolve argument

--- js/task10.js
+++ js/task10.js
@@ -1,19 +1,20 @@
-const data = [
-  {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
-  {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
-]
-
 const ul = document.querySelector('#lists');
 
 // loader画像
 const loader = document.createElement('img');
 loader.src = "./img/loading-circle.gif";
 
+//resolveになるまでの間にloading画像を出す
+ul.appendChild(loader);
+
 function fetchData() {
 
   return new Promise((resolve,reject) => {
       setTimeout(() => {
-        resolve(data);
+        resolve([
+          {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+          {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+        ]);
       }, 3000)
   });
 
@@ -22,12 +23,10 @@ function fetchData() {
 async function renderData() {
 
   try {
-    //resolveになるまでの間にloading画像を出す
-    ul.appendChild(loader);
 
-    await fetchData(data);
+    const response = await fetchData();
 
-    const markup = data.reduce((prev, current) => {
+    const markup = response.reduce((prev, current) => {
       return `${prev}<li><a href="${current.to}"><img src="${current.img}" alt="${current.alt}">${current.text}</a></li>`;
     }, "")
     ul.innerHTML = markup;
