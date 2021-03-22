bba3764b3e9820405e2b6f7cac3c646ebe47bfd7
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-03-22 21:43:43 +0900

    fix: Correct setTimeout function

--- js/task11.js
+++ js/task11.js
@@ -5,31 +5,29 @@ const loader = document.createElement('img');
 loader.src = "./img/loading-circle.gif";
 
 async function fetchData() {
-
-  const response = await fetch('https://jsondata.okiba.me/v1/json/zPMMU210321111158');
-  const data = response.json();
-
-  return new Promise((resolve,reject) => {
-    setTimeout(() => {
-      resolve(data);
-    }, 3000)
-  });
-
+  const response = await fetch('https://jsondata.okiba.me/v1/json/zPMMU210321111158')
+  return await response.json();
 }
+
+function timerForFetchingData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(function() {
+      resolve(fetchData());
+    }, 3000)
+  })
+}
+
 
 async function renderData() {
 
-  //resolveになるまでの間にloading画像を出す
+  // fetchDataで取得したデータを入れる変数
+  let data;
+
+  //loading画像を出す
   ul.appendChild(loader);
 
   try {
-
-    const data = await fetchData();
-
-    const markup = data.reduce((prev, current) => {
-      return `${prev}<li>${current.id} - ${current.name} - ${current.tel}</li>`;
-    }, "")
-    ul.innerHTML = markup;
+    data = await timerForFetchingData();
   }
   catch(error) {
     console.log(error);
@@ -37,6 +35,11 @@ async function renderData() {
   finally {
     ul.removeChild(loader);
   }
+
+  const markup = data.reduce((prev, current) => {
+    return `${prev}<li>${current.id} - ${current.name} - ${current.tel}</li>`;
+  }, "")
+  ul.innerHTML = markup;
 }
 
 renderData();
