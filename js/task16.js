64bca2af242e3544c63039adedc67d7823a83e78
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-06-10 15:59:58 -0700

    feat: add category switch buttons

--- js/task16.js
+++ js/task16.js
@@ -1,25 +1,36 @@
-// それぞれのカテゴリタブを開くことができてそれぞれのジャンルに応じた記事が4つ表示できる。(記事のタイトル名は適当)
+// DONE - それぞれのカテゴリタブを開くことができてそれぞれのジャンルに応じた記事が4つ表示できる。(記事のタイトル名は適当)
 
-// それぞれのカテゴリにはそれぞれ固有の画像が入る(右側四角。画像は適当)
+// DONE - それぞれのカテゴリにはそれぞれ固有の画像が入る(右側四角。画像は適当)
 
 // DONE - 記事にはnewという新着かどうかのラベルがつく(どこの記事にそれが入るかは適当でいいです)
 
 // DONE - 記事にはそれぞれコメントがあり、0件なら表示しない、1以上ならアイコンと共に数字が表示される
 
-// カテゴリタブは切り替えられる。面倒なら2つのカテゴリだけでよいです。その場合ニュースと経済だけにします
-  // タブ切り替え用のボタンを作る。ボタンにeventListenerを付けて、各カテゴリーのデータをfetchする
-  // そのボタンは、createElementで作る
+// DONE - カテゴリタブは切り替えられる。面倒なら2つのカテゴリだけでよいです。その場合ニュースと経済だけにします
+  // DONE - タブ切り替え用のボタンを作る。ボタンにeventListenerを付けて、各カテゴリーのデータをfetchする
+  // DONE - そのボタンは、createElementで作る
 
-// どのカテゴリタブを初期表示時に選んでいるかはデータとして持っている
+// DONE - どのカテゴリタブを初期表示時に選んでいるかはデータとして持っている
 
-// htmlはulだけ作ってあとはcreateElementで作る
+// DONE - htmlはulだけ作ってあとはcreateElementで作る
 
 // DONE - try-catchでエラー時はulの中に「ただいまサーバー側で通信がぶっ壊れています」みたいなテキストを画面内に表示すること
 
 const ul = document.getElementById('lists');
 
-const btn = document.createElement('button');
-btn.innerHTML = "News";
+const btn1 = document.createElement('button');
+btn1.id = 'news';
+btn1.innerHTML = "ニュース";
+
+const btn2 = document.createElement('button');
+btn2.id = 'economy';
+btn2.innerHTML = "経済";
+
+const image1 = document.createElement('img');
+image1.src = "./img/oli.jpg";
+
+const image2 = document.createElement('img');
+image2.src = "./img/economy.jpg";
 
 const today = new Date();
 const date = `${today.getFullYear()}${(today.getMonth()+1) < 10 ? `0` + (today.getMonth()+1) : (today.getMonth()+1) }${today.getDate() < 10 ? `0` + today.getDate() : today.getDate()}`;
@@ -48,18 +59,34 @@ async function getNewsData() {
     ul.innerHTML = "サーバーから情報を取得できませんでした。"
   }
   finally {
-    console.log("finally")
+    console.log("サーバーから情報が取得できました")
     ul.removeChild(loader);
   }
 }
 
 const addDataToInnerHTML = data => {
-  ul.appendChild(btn);
   const createNode = data.reduce((prev, current) => `${prev}<li>${current.title} ${date - (current.published.split('-').join('')) <= 14 ? "NEW" : ""} ${ (date - current.published) <= 14 ? `New` : '' } ${current.comment >= 1 ? `<img src="./comment.png" alt="comment icon" />` + current.comment : ''}</li>`, "");
   ul.innerHTML = createNode;
+  ul.prepend(btn1, btn2);
 }
 
 window.addEventListener('load', async (event) => {
   const data = await getNewsData();
-  addDataToInnerHTML(data);
+  const newsData = data.filter(el => el.category === 'news')
+  addDataToInnerHTML(newsData);
+  ul.appendChild(image1);
 });
+
+btn1.addEventListener('click', async function() {
+  const data = await getNewsData();
+  const newsData = data.filter(el => el.category === 'news')
+  addDataToInnerHTML(newsData);
+  ul.appendChild(image1);
+})
+
+btn2.addEventListener('click', async function() {
+  const data = await getNewsData();
+  const newsData = data.filter(el => el.category === 'economy')
+  addDataToInnerHTML(newsData);
+  ul.appendChild(image2);
+})
