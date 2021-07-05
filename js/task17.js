4a5a809a366161a9ed46bce0b2527ca6be566655
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-07-05 16:50:40 -0700

    fix: update createPrevButton/createNextButton function

--- js/task17.js
+++ js/task17.js
@@ -1,26 +1,38 @@
 const ul = document.getElementById('lists');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
 
-// loader
-const loader = document.createElement('img');
-loader.src = "./img/loading-circle.gif";
+let currentImage = 0;
+
+function createLoader() {
+  const fragment = document.createDocumentFragment();
+  const loaderContainer = document.createElement('li')
+  const loader = document.createElement('img');
+  loader.src = "./img/loading-circle.gif";
+  fragment.appendChild(loader);
+  loader.appendChild(loaderContainer)
+  loader.classList.add('loader_image');
+  return loader;
+}
 
 function fetchData() {
   return new Promise((resolve, reject) => {
-
     setTimeout(() => {
       resolve(fetch('./js/json/task17.json'));
     }, 3000)
-    
   })
 }
 
 async function getData() {
+  const loaderImage = createLoader();
 
-  ul.appendChild(loader);
+  const fragment = document.createDocumentFragment();
+  const li = document.createElement('li');
+  li.classList.add('loader')
 
+  li.appendChild(loaderImage)
+  fragment.appendChild(li)
+   
   try {
+    ul.appendChild(fragment);
     const response = await fetchData();
     return await response.json();
   }
@@ -29,24 +41,23 @@ async function getData() {
   }
   finally {
     console.log("You got the result.")
-    ul.removeChild(loader);
+    ul.removeChild(li);
   }
 }
 
-let currentImage = 0;
-
 function displayImage(res) {
   const data = res.data;  
-
+  const loaderImage = createLoader();
   const fragment = document.createDocumentFragment();
-  
+
   const image = document.createElement('img');
   const li = document.createElement('li');
-  li.classList.add('active')
-  image.src = data[currentImage].image;    
-
-  li.appendChild(image)
-  fragment.appendChild(li)
+  li.classList.add('active');
+  image.src = data[currentImage].image;
+ 
+  li.appendChild(image);
+  fragment.appendChild(li);
+  ul.innerHTML = "";
   ul.appendChild(fragment);
 }
 
@@ -56,49 +67,50 @@ function createPrevButton() {
   prevBtn.innerHTML = "Prev";
   prevBtn.disabled = true;
   ul.parentNode.insertBefore(prevBtn, ul);
-  console.log("prevBtn", prevBtn);
 }
 
 function createNextButton() {
   const nextBtn = document.createElement('button');
   nextBtn.id = "next";
   nextBtn.innerHTML = "Next";
-  nextBtn.disabled = false;
   ul.parentNode.appendChild(nextBtn);
-  console.log("nextBtn", nextBtn);
 }
 
-if(prevBtn) {
-  prevBtn.addEventListener('click', async function(e) {
-    console.log("prevBtn was cllicked");
-    const res = await getData();
-  
+createPrevButton();
+createNextButton();
+
+const prevBtn = document.getElementById('prev');
+prevBtn.addEventListener('click', async function(e) {
+  const res = await getData();
+
+  e.target.disabled = true;
+
+  if(currentImage > 0) {
+    e.target.disabled = false;
+    nextBtn.disabled = false;
+    currentImage--;
+    displayImage(res);
+  }
+  if(currentImage === 0) {
     e.target.disabled = true;
-  
-    if(currentImage > 0) {
-      currentImage--;
-      displayImage(res);
-    }
-  })
-}
+  }  
+})
 
-if(nextBtn) {
-  nextBtn.addEventListener('click', async function() {
-    console.log("nextBtn was cllicked");
+const nextBtn = document.getElementById('next');
+nextBtn.addEventListener('click', async function(e) {
+  const res = await getData();
 
-    const res = await getData();
-  
-    if(currentImage < res.length - 1) {
-      currentImage++;
-      console.log("currentImage", currentImage)
-      displayImage(res);
-    }
-  })
-}
+  if(currentImage < res.data.length - 1) {
+    prevBtn.disabled = false;
+    currentImage++;
+    displayImage(res);
+  }
+  if(currentImage === 4) {
+    e.target.disabled = true;
+  }
+})
 
 document.addEventListener("DOMContentLoaded", async function() {
-  createPrevButton();
-  createNextButton();
   const data = await getData();
   displayImage(data);    
 });
