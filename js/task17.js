67ae557c5c1677b35a53eab8d9bef7ab03674e05
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-09-06 21:45:31 -0700

    fix: fix displayImage function

--- js/task17.js
+++ js/task17.js
@@ -1,123 +1,129 @@
 const ul = document.getElementById('lists');
 
 let currentImage = 0;
+const imagesArr = [];
 
 function createLoader() {
-  const fragment = document.createDocumentFragment();
-  const loaderContainer = document.createElement('li')
-  const loader = document.createElement('img');
-  loader.src = "./img/loading-circle.gif";
-  fragment.appendChild(loader);
-  loader.appendChild(loaderContainer)
-  loader.classList.add('loader_image');
-  return loader;
+	const fragment = document.createDocumentFragment();
+	const loaderContainer = document.createElement('li');
+	const loader = document.createElement('img');
+	loader.src = './img/loading-circle.gif';
+	fragment.appendChild(loader);
+	loader.appendChild(loaderContainer);
+	loader.classList.add('loader_image');
+	return loader;
 }
 
 function fetchData() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(fetch('./js/json/task17.json'));
-    }, 3000)
-  })
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(fetch('./js/json/task17.json'));
+		}, 3000);
+	});
 }
 
 async function getData() {
-  const loaderImage = createLoader();
+	const loaderImage = createLoader();
 
-  const fragment = document.createDocumentFragment();
-  const li = document.createElement('li');
-  li.classList.add('loader')
+	const fragment = document.createDocumentFragment();
+	const li = document.createElement('li');
+	li.classList.add('loader');
 
-  li.appendChild(loaderImage)
-  fragment.appendChild(li)
-  ul.appendChild(fragment)
-   
-  try {
-    ul.appendChild(fragment);
-    const response = await fetchData();
-    const res = await response.json();
-    const data = res.data;  
-    return data;
-  }
-  catch(error) {
-    ul.innerHTML = "Couldn't get response."
-  }
-  finally {
-    console.log("You got the result.")
-    ul.removeChild(li);
-  }
+	li.appendChild(loaderImage);
+	fragment.appendChild(li);
+	console.log('fragment', fragment);
+	ul.appendChild(fragment);
+
+	try {
+		const response = await fetchData();
+		const res = await response.json();
+		const data = res.data;
+
+		data.map((el) => {
+			const image = document.createElement('img');
+			const li = document.createElement('li');
+
+			image.src = el.image;
+			li.appendChild(image);
+
+			imagesArr.push(li);
+		});
+
+		imagesArr.forEach((el, index) => {
+			el.classList.add('slide-images');
+
+			if (index === 0) {
+				el.classList.add('z-index-100');
+			}
+			ul.appendChild(el);
+		});
+	} catch (error) {
+		ul.innerHTML = "Couldn't get response.";
+	} finally {
+		console.log('You got the result.');
+	}
 }
+getData();
 
-async function displayImage(currentImage) {
-  const data = await getData();
+function displayImage(currentImage, clickEvent) {
+	let imageShown, nextImage;
+	let btnType = clickEvent.id;
 
-  const loaderImage = createLoader();
-  const fragment = document.createDocumentFragment();
+	if (btnType === 'next') {
+		imageShown = imagesArr[currentImage];
+		nextImage = imagesArr[currentImage - 1];
 
-  let images = data.map(el => {
+		imageShown.classList.add('z-index-100');
+		nextImage.classList.remove('z-index-100');
+	} else if (btnType === 'prev') {
+		imageShown = imagesArr[currentImage];
+		prevImage = imagesArr[currentImage + 1];
 
-    const image = document.createElement('img');
-    const li = document.createElement('li');
-    li.classList.add('active');
-    
-    image.src = el.image;
-    li.appendChild(image);
-  
-    return li;
-  })
-
-  fragment.appendChild(images[currentImage]);
-  ul.innerHTML = "";
-  ul.appendChild(fragment);
-  
+		imageShown.classList.add('z-index-100');
+		prevImage.classList.remove('z-index-100');
+	}
 }
 
 function createPrevButton() {
-  const prevBtn = document.createElement('button');
-  prevBtn.id = "prev";
-  prevBtn.innerHTML = "Prev";
-  prevBtn.disabled = true;
-  ul.parentNode.insertBefore(prevBtn, ul);
+	const prevBtn = document.createElement('button');
+	prevBtn.id = 'prev';
+	prevBtn.innerHTML = 'Prev';
+	prevBtn.disabled = true;
+	ul.parentNode.insertBefore(prevBtn, ul);
 }
 
 function createNextButton() {
-  const nextBtn = document.createElement('button');
-  nextBtn.id = "next";
-  nextBtn.innerHTML = "Next";
-  ul.parentNode.appendChild(nextBtn);
+	const nextBtn = document.createElement('button');
+	nextBtn.id = 'next';
+	nextBtn.innerHTML = 'Next';
+	ul.parentNode.appendChild(nextBtn);
 }
 
 createPrevButton();
 createNextButton();
 
 const prevBtn = document.getElementById('prev');
-prevBtn.addEventListener('click', async function(e) {
+prevBtn.addEventListener('click', function (e) {
+	e.target.disabled = true;
 
-  e.target.disabled = true;
-
-  if(currentImage > 0) {
-    e.target.disabled = false;
-    nextBtn.disabled = false;
-    currentImage--;
-    displayImage(currentImage);
-  }
-  if(currentImage === 0) {
-    e.target.disabled = true;
-  }  
-})
+	if (currentImage > 0) {
+		e.target.disabled = false;
+		nextBtn.disabled = false;
+		currentImage--;
+		displayImage(currentImage, e.target);
+	}
+	if (currentImage === 0) {
+		e.target.disabled = true;
+	}
+});
 
 const nextBtn = document.getElementById('next');
-nextBtn.addEventListener('click', async function(e) {
+nextBtn.addEventListener('click', function (e) {
+	prevBtn.disabled = false;
+	currentImage++;
+	displayImage(currentImage, e.target);
 
-  prevBtn.disabled = false;
-  currentImage++;
-  displayImage(currentImage);
-
-  if(currentImage === 4) {
-    e.target.disabled = true;
-  }
-})
-
-document.addEventListener("DOMContentLoaded", async function() {
-  displayImage(currentImage);    
+	if (currentImage === 4) {
+		e.target.disabled = true;
+	}
 });
