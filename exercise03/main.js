0ffd9ba78cd352f3cc8602399a71ca35153da219
Kaya0312 <oyama_kayano@yahoo.co.jp>
2022-09-18 23:55:53 -0700

    Update Exercise 3

--- exercise03/main.js
+++ exercise03/main.js
@@ -1,20 +1,20 @@
 const data = [
-  {
-    href: "a1.html",
-    src: "/img/bookmark.png",
-    text: "a1"
-  },
-  {
-    href: "a2.html",
-    src:"/img/bookmark.png",
-    text: "a2"
-  }
-]
+	{
+		href: 'a1.html',
+		src: 'images/bookmark.png',
+		text: 'a1',
+	},
+	{
+		href: 'a2.html',
+		src: 'images/bookmark.png',
+		text: 'a2',
+	},
+];
 
 const ul = document.querySelector('#lists');
 
 const template = data.reduce((prev, current) => {
-  return `${prev}<li><a href=${current.href}><img src=${current.src}>${current.text}</a></li>`;
-}, "")
+	return `${prev}<li><a href=${current.href}><img src=${current.src}>${current.text}</a></li>`;
+}, '');
 
 ul.innerHTML = template;
