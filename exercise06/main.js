af9f13089ac24b900383c7998b5eb7d6726a475e
Kaya0312 <oyama_kayano@yahoo.co.jp>
2022-09-21 19:37:21 -0700

    Update Exercise 6

--- exercise06/main.js
+++ exercise06/main.js
@@ -1,22 +1,20 @@
 const data = [
-  {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
-  {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
-]
+	{ to: 'bookmark.html', img: '1.png', alt: 'Image 1', text: 'bookmark' },
+	{ to: 'message.html', img: '2.png', alt: 'Image 2', text: 'message' },
+];
 
-const promise = new Promise((resolve,reject) => {
-  setTimeout(() => {
-    resolve(data);
-  }, 3000)
+const promise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve(data);
+	}, 3000);
 });
 
 const ul = document.querySelector('#lists');
 
-promise.then(response => {
+promise.then((response) => {
+	const markup = response.reduce((prev, current) => {
+		return `${prev}<li><a href="${current.to}"><img src="${current.img}" alt="${current.alt}">${current.text}</a></li>`;
+	}, '');
 
-  const markup = response.reduce((prev, current) => {
-    return `${prev}<li><a href="${current.to}"><img src="${current.img}" alt="${current.alt}">${current.text}</a></li>`;
-  }, "")
-
-  ul.insertAdjacentHTML('afterbegin', markup);
-
-})
+	ul.insertAdjacentHTML('afterbegin', markup);
+});
