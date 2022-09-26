6c2a77d52fbb7faee36b16dfb5eb3637405df0e6
Kaya0312 <oyama_kayano@yahoo.co.jp>
2022-09-25 23:53:08 -0700

    Update Exercise 9

--- exercise09/main.js
+++ exercise09/main.js
@@ -1,39 +1,33 @@
 const data = [
-  {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
-  {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
-]
+	{ to: 'bookmark.html', img: '1.png', alt: 'Image 1', text: 'bookmark' },
+	{ to: 'message.html', img: '2.png', alt: 'Image 2', text: 'message' },
+];
 
 const ul = document.getElementById('lists');
 
-// loader画像
+// loader
 const loader = document.createElement('img');
-loader.src = "./img/loading-circle.gif";
+loader.src = 'loading-circle.gif';
 
-//resolveになるまでの間にloading画像を出す
 ul.appendChild(loader);
 
 function fetchData() {
-
-  return new Promise((resolve,reject) => {
-    setTimeout(() => {
-      resolve(data);
-    }, 3000)
-  });
-
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(data);
+		}, 3000);
+	});
 }
 
 async function renderData() {
+	await fetchData(data);
 
-  await fetchData(data);
+	ul.removeChild(loader);
 
-  //終わったら除く
-  ul.removeChild(loader);
-
-  const markup = data.reduce((prev, current) => {
-    return `${prev}<li><a href="${current.to}"><img src="${current.img}" alt="${current.alt}">${current.text}</a></li>`;
-  }, "")
-  ul.innerHTML = markup;
-
+	const markup = data.reduce((prev, current) => {
+		return `${prev}<li><a href="${current.to}"><img src="${current.img}" alt="${current.alt}">${current.text}</a></li>`;
+	}, '');
+	ul.innerHTML = markup;
 }
 
 renderData();
