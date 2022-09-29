77095a6f02ab2702a89fd22378b88dc3d10e40b9
Kaya0312 <oyama_kayano@yahoo.co.jp>
2022-09-28 21:34:33 -0700

    Update Exercise 12

--- exercise12/main.js
+++ exercise12/main.js
@@ -1,41 +1,36 @@
 const ul = document.getElementById('lists');
 const btn = document.getElementById('btn');
 
-// loader画像
+// loader
 const loader = document.createElement('img');
-loader.src = "./img/loading-circle.gif";
+loader.src = 'loading-circle.gif';
 
 function fetchData() {
-  return new Promise((resolve, reject) => {
-    setTimeout(function() {
-      resolve(fetch('https://jsondata.okiba.me/v1/json/zPMMU210321111158'));
-    }, 3000)
-  })
+	return new Promise((resolve, reject) => {
+		setTimeout(function () {
+			resolve(fetch('https://jsondata.okiba.me/v1/json/zPMMU210321111158'));
+		}, 3000);
+	});
 }
 
 async function renderData() {
+	let data;
 
-  // fetchDataで取得したデータを入れる変数
-  let data;
+	ul.appendChild(loader);
 
-  //loading画像を出す
-  ul.appendChild(loader);
+	try {
+		const response = await fetchData();
+		data = await response.json();
+	} catch (error) {
+		console.log(error);
+	} finally {
+		ul.removeChild(loader);
+	}
 
-  try {
-    const response = await fetchData();
-    data = await response.json();
-  }
-  catch(error) {
-    console.log(error);
-  }
-  finally {
-    ul.removeChild(loader);
-  }
-
-  const markup = data.reduce((prev, current) => {
-    return `${prev}<li>${current.id} - ${current.name} - ${current.tel}</li>`;
-  }, "")
-  ul.innerHTML = markup;
+	const markup = data.reduce((prev, current) => {
+		return `${prev}<li>${current.id} - ${current.name} - ${current.tel}</li>`;
+	}, '');
+	ul.innerHTML = markup;
 }
 
 btn.addEventListener('click', renderData);
