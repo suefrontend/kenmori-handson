7ce8a196a067d59a157c7edb44a362728793d5cd
Kaya0312 <oyama_kayano@yahoo.co.jp>
2022-11-06 00:24:21 -0700

    feat: Create slider dots

--- exercise18/main.js
+++ exercise18/main.js
@@ -1,5 +1,8 @@
 // Access to DOMs
 const ul = document.getElementById('js-lists');
+const dotContainer = document.getElementById('js-dots');
+
+let numOfImages = 0;
 
 // Show/remove loader
 const showLoader = () => {
@@ -30,7 +33,6 @@ const getData = async () => {
 		const response = await fetchData();
 		const res = await response.json();
 		const data = res.data;
-		console.log('data', data);
 		return data;
 	} catch (error) {
 		ul.innerHTML = "Couldn't get response.";
@@ -54,8 +56,29 @@ const createNextButton = () => {
 	ul.parentNode.appendChild(nextBtn);
 };
 
+const createDots = () => {
+	const fragment = document.createDocumentFragment();
+
+	for (let i = 0; i < numOfImages; i++) {
+		let dot = document.createElement('li');
+		dot.classList.add('slide__dot__item');
+		fragment.appendChild(dot);
+	}
+
+	dotContainer.appendChild(fragment);
+};
+
 document.addEventListener('DOMContentLoaded', async function () {
 	const data = await getData();
+	numOfImages = data.length;
 	createPrevButton();
 	createNextButton();
+	createDots();
 });
+
+// やること
+// -- 画像をul内に表示させる関数を作る
+// -- 表示させる関数を3秒ごとに呼び出す
+// -- 画像の数だけドットを生成 createDotButtons()
+// -- ドットをクリックすると、画像を表示させる関数が呼び出される
+// -- 現在の画像の順番のUI部分をアップデートする
